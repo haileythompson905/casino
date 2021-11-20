1f433bbf4c6212a3afe4b26626824a3516feebe9
Hailey Thompson <haileythompson905@yahoo.com>
2021-11-20 14:51:46 -0700

    initial commit

---
+++ card.rb
@@ -0,0 +1,30 @@
+class Card
+  # Getter and Setter methods for rank, suit and color
+  attr_accessor :rank, :suit, :color
+  # Gets called when you call the new method to create an instance
+  # card = Card.new('10', 'Spades', 'Black')
+  def initialize(rank, suit, color)
+    @rank = rank
+    @suit = suit
+    @color = color
+  end
+ end
+
+ def to_i
+  case @rank
+    When "A"
+    return 1
+    When "J" 
+    return 11
+    When "Q"
+    return 12
+    When "K"
+    return 13
+  else @rank.to_i
+  end
+end
+
+def to_s
+  return "#{@rank} #{@suit} (#{color})"
+end
+end
---
+++ player.rb
@@ -0,0 +1,8 @@
+class Player
+  attr_accessor :name, :money
+def initialize 
+  puts "What is your name?"
+  @name = gets.strip
+  @money = 250.0
+end
+end
